7d70105761cbb25de3bf7fd061e64065c415f438
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-08-28 14:53:11 -0300

    prototipo

---
+++ Estudos/html-css/ex001/script.js
@@ -0,0 +1,14 @@
+
+function entrar() {
+    var login = document.getElementById('itexto').value
+    var senha = document.getElementById('isenha').value
+    var lista = document.getElementById('ilista').value
+
+    if (login == 'felipe' && senha == '123') {
+        alert('entrou')
+        location.href = 'prototipo02.html'
+
+    } else {
+        alert('login invalido')
+    }
+}
