8f691493a10888d84d8ca0a66b6f40934e4bd416
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2022-10-01 08:15:27 -0300

    Clock

--- Estudos/html-css/ex042 projeto-relogio/script/script.js
+++ Estudos/html-css/ex042 projeto-relogio/script/script.js
@@ -1,3 +1,18 @@
 
+var box = document.getElementById('hours')
+var box2 = document.getElementById('minutes')
+var box3 = document.getElementById('seconds')
 
+var relogio = setInterval(function time() {
+    var hora = new Date().getHours()
+    var minutes = new Date().getMinutes()
+    var seconds = new Date().getSeconds()
 
+    if (hora < 10) hora = '0' + hora;
+    if (minutes < 10) minutes = '0' + minutes;
+
+    box.querySelector('span').textContent = hora
+    box2.querySelector('span').textContent = minutes
+    box3.querySelector('span').textContent = seconds
+
+})
